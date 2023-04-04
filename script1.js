05ee30337a5efbf63834665a2b00c2d1c1fe6c1e
Vladislav7161 <125801572+Vladislav7161@users.noreply.github.com>
2023-04-04 17:56:31 +0300

    Update script1.js

--- script1.js
+++ script1.js
@@ -3,8 +3,20 @@ const hour1 = document.getElementById("hour");
 const minute1 = document.getElementById("minute");
 const ampm1 = document.getElementById("ampm");
 const citySelect1 = document.getElementById("city");
-const formatSelect1 = document.getElementById("format");
+const format1 = document.getElementById("format");
 
+
+const cityBackgrounds = {
+  "Kiev": "C:/Users/admin/Documents/GitHub/JavaScript/kiev_background.jpg",
+  "London": "C:/Users/admin/Documents/GitHub/JavaScript/london_background.jpg", 
+  "New York": "C:/Users/admin/Documents/GitHub/JavaScript/newyork_background.jpg",
+  "Berlin": "C:/Users/admin/Documents/GitHub/JavaScript/berlin_background.jpg"
+}
+
+function updateBackground(city) {
+  const background = cityBackgrounds.hasOwnProperty(city) ? cityBackgrounds[city] : "C:/Users/admin/Documents/GitHub/JavaScript/default_background.jpg";
+  document.body.style.backgroundImage = `url(${background})`;
+}
 
 function updateTime(city) {
   const now = new Date();
@@ -13,22 +25,27 @@ function updateTime(city) {
   day1.innerText = day;
 
   const cityOffsets = {
-    "Kiev" : 0,
-    "London" : -2,
-    "New York" : -7,
-    "Berlin" : -1
-
+    "Kiev": 0,
+    "London": -2,
+    "New York": -7,
+    "Berlin": -1
   }
 
   const cityOffset = cityOffsets.hasOwnProperty(city) ? cityOffsets[city] : 0;
   const utc = now.getTime() + (now.getTimezoneOffset());
   const cityTime = new Date(utc + (3600000 * cityOffset));
-  
-  
+
   let hours = cityTime.getHours();
-  hours = hours % 12;
-  hours = hours ? hours : 12;
-  hour1.innerText = hours;
+  let format = format1.value;
+  if (format === "12") {
+    let ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    ampm1.innerText = ampm;
+  } else if (format === "24") {
+    ampm1.innerText = "";
+  }
+  hour1.innerText = hours < 10 ? "0" + hours : hours;
 
   let minutes = cityTime.getMinutes();
   if (minutes < 10) {
@@ -36,14 +53,18 @@ function updateTime(city) {
   }
   minute1.innerText = minutes;
 
-  let ampm = hours >= 12 ? 'PM' : 'AM';
-  ampm1.innerText = ampm;
+
 }
 
-updateTime();
+updateTime(citySelect1.value);
 
 citySelect1.addEventListener("change", () => {
   const city = citySelect1.value;
+  updateBackground(city);
   updateTime(city);
 });
 
+format1.addEventListener("change", () => {
+  const city = citySelect1.value;
+  updateTime(city);
+});
